807bf6db351ba6a30dffb5d4536e7448d9c6a0d9
tarathep <tarathep.choengchaiyaphum@apac.nttdata.com>
2024-02-06 17:46:02 +0700

    7 router redirect

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,11 @@ import { createApp } from "vue";
 import "./style.css";
 import App from "./App.vue";
 import Antd from "ant-design-vue";
+import router from "./router";
 
 // createApp(App).mount('#app')
 
 const app = createApp(App);
+app.use(router);
 app.use(Antd);
 app.mount("#app");
---
+++ src/router/index.ts
@@ -0,0 +1,38 @@
+import * as vueRouter from "vue-router";
+import Login from "../views/Login.vue";
+import Register from "../views/Register.vue";
+import Stock from "../views/Stock.vue";
+
+const routes: Array<vueRouter.RouteRecordRaw> = [
+  {
+    path: "/login",
+    name: "login",
+    component: Login,
+  },
+  {
+    path: "/register",
+    name: "register",
+    component: Register,
+  },
+  {
+    path: "/stock",
+    meta: { isSecured: true },
+    name: "stock",
+    component: Stock,
+  },
+  {
+    path: "/",
+    redirect: "/login",
+  },
+  {
+    path: "/:pathMatch(.*)*",
+    redirect: "/login", // page not found
+  },
+];
+
+const router = vueRouter.createRouter({
+  history: vueRouter.createWebHistory(import.meta.env.BASE_URL),
+  routes,
+});
+
+export default router;
