ebdc100ba9c1d1c9fb7c839d316cbc8252ad1155
tarathep <tarathep.choengchaiyaphum@apac.nttdata.com>
2024-02-06 15:51:00 +0700

    init project

---
+++ README.md
@@ -0,0 +1,4 @@
+# commands
+
+- node --version
+- yarn --version
---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <h1>NTT: {{ tmp }}</h1>
+    <button @click="clickMe">click Me</button>
+
+    <a-button type="primary">Primary Button</a-button>
+    <a-button>Default Button</a-button>
+    <a-button type="dashed">Dashed Button</a-button>
+    <a-button type="text">Text Button</a-button>
+    <a-button type="link">Link Button</a-button>
+
+    <a-button type="primary">
+      <template #icon><SearchOutlined /></template>
+      Search
+    </a-button>
+  </div>
+</template>
+
+<script lang="ts">
+import SearchOutlined from "@ant-design/icons-vue/SearchOutlined";
+
+export default {
+  components: { SearchOutlined },
+  // lanmda function
+  setup: () => {
+    const tmp = "Bokie";
+
+    const clickMe = () => {
+      alert("Hey");
+    };
+
+    return { tmp, clickMe };
+  },
+};
+</script>
+
+<style></style>
