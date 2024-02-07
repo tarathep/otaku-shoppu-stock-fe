1662f497c595b2df91917d7b13ed39ce2b9326d4
tarathep <tarathep.choengchaiyaphum@apac.nttdata.com>
2024-02-07 18:04:01 +0700

    12 Separate Content

--- src/App.vue
+++ src/App.vue
@@ -11,7 +11,7 @@
           minHeight: '280px',
         }"
       >
-        Content
+        <Content />
       </a-layout-content>
     </a-layout>
   </a-layout>
@@ -20,6 +20,7 @@
 import { ref } from "vue";
 import Menu from "./components/core/Menu.vue";
 import Header from "./components/core/Header.vue";
+import Content from "./components/core/Content.vue";
 
 const collapsed = ref<boolean>(false);
 </script>
--- src/components/core/Content.vue
+++ src/components/core/Content.vue
@@ -1,5 +1,7 @@
 <template>
-  <RouterView />
+  <div class="flex flex-col justify-center items-center">
+    <router-view />
+  </div>
 </template>
 
 <script lang="ts">
