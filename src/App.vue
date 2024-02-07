c9b556a03fd16bd1f6b7ff3d984310f041bc8707
tarathep <tarathep.choengchaiyaphum@apac.nttdata.com>
2024-02-07 16:45:53 +0700

    10 Separate Menu

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,6 @@
 <template>
   <a-layout class="h-screen">
-    <a-layout-sider v-model:collapsed="collapsed" :trigger="null" collapsible>
-      <div class="logo" />
-      <a-menu v-model:selectedKeys="selectedKeys" theme="dark" mode="inline">
-        <a-menu-item key="1">
-          <user-outlined />
-          <span>nav 1</span>
-        </a-menu-item>
-        <a-menu-item key="2">
-          <video-camera-outlined />
-          <span>nav 2</span>
-        </a-menu-item>
-        <a-menu-item key="3">
-          <upload-outlined />
-          <span>nav 3</span>
-        </a-menu-item>
-      </a-menu>
-    </a-layout-sider>
+    <Menu v-model:collapsed="collapsed" />
     <a-layout>
       <a-layout-header style="background: #fff; padding: 0">
         <menu-unfold-outlined
@@ -45,15 +29,9 @@
 </template>
 <script lang="ts" setup>
 import { ref } from "vue";
-import {
-  UserOutlined,
-  VideoCameraOutlined,
-  UploadOutlined,
-  MenuUnfoldOutlined,
-  MenuFoldOutlined,
-} from "@ant-design/icons-vue";
+import Menu from "./components/core/Menu.vue";
+import { MenuUnfoldOutlined, MenuFoldOutlined } from "@ant-design/icons-vue";
 
-const selectedKeys = ref<string[]>(["1"]);
 const collapsed = ref<boolean>(false);
 </script>
 <style>
@@ -67,12 +45,6 @@ const collapsed = ref<boolean>(false);
 
 #components-layout-demo-custom-trigger .trigger:hover {
   color: #1890ff;
-}
-
-#components-layout-demo-custom-trigger .logo {
-  height: 32px;
-  background: rgba(255, 255, 255, 0.3);
-  margin: 16px;
 }
 
 .site-layout .site-layout-background {
