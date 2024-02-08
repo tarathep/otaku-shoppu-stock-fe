58da5c53d99a6c8d5daf81b6c6f1330d88d6ec0e
tarathep <tarathep.choengchaiyaphum@apac.nttdata.com>
2024-02-08 10:36:16 +0700

    14 tsconfig Alias Import

--- src/App.vue
+++ src/App.vue
@@ -18,7 +18,7 @@
 </template>
 <script lang="ts" setup>
 import { ref } from "vue";
-import Menu from "./components/core/Menu.vue";
+import Menu from "@/components/core/Menu.vue";
 import Header from "./components/core/Header.vue";
 import Content from "./components/core/Content.vue";
 
