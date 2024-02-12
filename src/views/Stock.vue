4ddda5bddc8cf3aba445372f2c441b8a09b503cc
tarathep <tarathep.choengchaiyaphum@apac.nttdata.com>
2024-02-12 22:10:12 +0700

    44 stock 1

--- src/views/Stock.vue
+++ src/views/Stock.vue
@@ -1,11 +1,43 @@
 <template>
-  <h1>Stock</h1>
+  <a-table :dataSource="dataSource" :columns="columns" />
 </template>
+<script>
+  export default {
+    setup() {
+      return {
+        dataSource: [
+          {
+            key: '1',
+            name: 'Mike',
+            age: 32,
+            address: '10 Downing Street',
+          },
+          {
+            key: '2',
+            name: 'John',
+            age: 42,
+            address: '10 Downing Street',
+          },
+        ],
 
-<script lang="ts">
-export default {
-  name: "Stock",
-};
+        columns: [
+          {
+            title: 'Name',
+            dataIndex: 'name',
+            key: 'name',
+          },
+          {
+            title: 'Age',
+            dataIndex: 'age',
+            key: 'age',
+          },
+          {
+            title: 'Address',
+            dataIndex: 'address',
+            key: 'address',
+          },
+        ],
+      };
+    },
+  };
 </script>
-
-<style></style>
