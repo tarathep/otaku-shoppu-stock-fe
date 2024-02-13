b87666e33adbd36eb69b3ab8879f853eaf41a7e0
tarathep <tarathep.choengchaiyaphum@apac.nttdata.com>
2024-02-13 17:04:29 +0700

    59. Stock 17 - Card Summary

--- src/views/Stock.vue
+++ src/views/Stock.vue
@@ -7,76 +7,14 @@
       :key="i"
       class="tw-mb-2 tw-px-2"
     >
-      <StockCard :title="item.title" :color="item.color" />
+      <StockCard
+        :title="item.title"
+        :amount="item.amount"
+        :color="item.color"
+        :icon="item.icon"
+      />
     </a-col>
   </a-row>
-
-  <!-- <div>
-    <a-table
-      :columns="columns"
-      :data-source="stockStore.stocks"
-      :pagination="{ position: ['topRight'] }"
-      :scroll="{
-        x: 'max-content',
-        y: '50vh',
-      }"
-    >
-      <template #headerCell="{ column }">
-        <span class="tw-font-bold"> {{ column.title }} </span>
-      </template>
-
-      <template #bodyCell="{ column, record }">
-        <template v-if="column.dataIndex === 'name'">
-          <div class="tw-block tw-truncate tw-overflow-ellipsis">
-            <a @click="routeToEdit(record.id)">
-              {{ record.name }}
-            </a>
-          </div>
-        </template>
-        <template v-else-if="column.dataIndex === 'image'">
-          <img
-            :src="stockStore.getProductImage(record.image)"
-            lazy-src="https://picsum.photos/id/11/10/6"
-            aspect-ratio="1"
-            class="tw-object-contain tw-cursor-pointer tw-transition-all hover:tw-scale-[1.1]"
-            width="50"
-            height="50"
-          />
-        </template>
-        <template v-else-if="column.dataIndex === 'price'">
-          <span>{{ filters.currency(record.price) }}</span>
-        </template>
-        <template v-else-if="column.dataIndex === 'stock'">
-          <a-tag>{{ record.stock }} pcs</a-tag>
-        </template>
-        <template v-else-if="column.dataIndex === 'createdAt'">
-          <span class="tw-text-gray-600">{{
-            filters.formatDate(record.createdAt)
-          }}</span>
-        </template>
-        <template v-else-if="column.dataIndex === 'updatedAt'">
-          <span class="tw-text-gray-600">{{ record.updatedAt }}</span>
-        </template>
-        <template v-else-if="column.dataIndex === 'action'">
-          <a-row align="center">
-            <a-button
-              class="tw-bg-[#ffd155ff] tw-border-[#ffd155ff] tw-text-white"
-              @click="$router.push(`/stock-edit/${record.id}`)"
-            >
-              <EditFilled class="tw-pb-2" />
-            </a-button>
-
-            <a-popconfirm title="Are you sure？">
-              <template #icon><QuestionCircleOutlined /></template>
-              <a-button type="danger">
-                <DeleteFilled class="tw-pb-2"
-              /></a-button>
-            </a-popconfirm>
-          </a-row>
-        </template>
-      </template>
-    </a-table>
-  </div> -->
 </template>
 <script lang="ts">
 import { useStockStore } from "@/stores/useStockStore";
